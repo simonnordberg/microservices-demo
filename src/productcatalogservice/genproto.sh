1268e7e0e4516ac8f2d2ab226dfd78c390637b98
Simon Nordberg <simon@simonnordberg.com>
2023-02-07 10:45:12 +0100

    Touch file to trigger build

--- src/productcatalogservice/genproto.sh
+++ src/productcatalogservice/genproto.sh
@@ -3,6 +3,9 @@
 PATH=$PATH:$GOPATH/bin
 protodir=../../proto
 
+# Source: https://github.com/grpc/grpc-go/blob/master/cmd/protoc-gen-go-grpc/README.md
+# Consider using --go-grpc_out=require_unimplemented_servers=false
+
 mkdir -p genproto
 protoc \
   --go_out=./genproto --go_opt=paths=source_relative \

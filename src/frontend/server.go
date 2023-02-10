4d65694f27872149f2d65dcdd8f7e1b898c117d2
Simon Nordberg <simon@simonnordberg.com>
2023-02-10 12:51:49 +0100

    Use grpclb

--- src/frontend/server.go
+++ src/frontend/server.go
@@ -82,7 +82,7 @@ func mustConnGRPC(conn **grpc.ClientConn, addr string) {
 
 	*conn, err = grpc.DialContext(ctx, addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithDefaultServiceConfig(`{"loadBalancingConfig": [{"grpclb":{}}]}`))
+		grpc.WithDefaultServiceConfig(`{"loadBalancingConfig":[{"grpclb":{"childPolicy":[{"round_robin":{}}]}}]}`))
 	if err != nil {
 		panic(errors.Wrapf(err, "grpc: failed to connect %s", addr))
 	}
